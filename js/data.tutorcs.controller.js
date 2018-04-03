dc5c6b78d532eeb5efb9dd25dfea7b5d00ea16dd
Cameron Gitzlaff <cameron-gitzlaff@uiowa.edu>
2018-04-03 18:42:33 -0500

    data controller

---
+++ js/data.tutorcs.controller.js
@@ -0,0 +1,9 @@
+/*
+* Controller the handles data across application
+*/
+(function () {
+	'use strict';
+	
+	// the 'tutorCS' part come from teh name of the app we created in tutorcs.module.js
+	var myApp = angular.module("tutorcs");
+})
