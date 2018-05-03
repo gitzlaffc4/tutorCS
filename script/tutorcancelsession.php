5e92d3bf6018454ae89e76724406b79416efa8fb
Murphy <cmurphy3@serv42.divms.uiowa.edu>
2018-05-03 05:16:09 -0500

    additional changes

--- script/tutorcancelsession.php
+++ script/tutorcancelsession.php
@@ -23,7 +23,7 @@ $data = json_decode(file_get_contents('php://input'), true);
 // get each piece of data
 
 // '' matches the '' attribute in the for
-$id = $data['id'];
+$AVAILSLOTID = $data['AVAILSLOTID'];
 
 //set up variables to handle errors
 // is complete will be false if we find any problems when checking on the data
@@ -39,7 +39,7 @@ $errorMessage = "";
 //Validation
 //
 
-if (!isset($id) && ((int)$id == $id)) {
+if (!isset($AVAILSLOTID) && ((int)$AVAILSLOTID == $AVAILSLOTID)) {
 	$isComplete = false;
 	$errorMessage .= "the tutorcancelseesion.php requires an integer id to be sent.";
 	
@@ -48,8 +48,24 @@ if (!isset($id) && ((int)$id == $id)) {
 // chekc if there is a record in th database matching the id
 if ($isComplete) {
 	// set up a query to chekc if the id passed to this file correspons to a record in the database
-	$deletequery = "SELECT HAWKID FROM AVAILABILITY_T WHERE AVAILSLOTID=$id";
+	$query = "SELECT HAWKID FROM AVAILABILITY_T WHERE AVAILSLOTID=$AVAILSLOTID";
 	
+	// run the check query
+	$result = queryDB($query, $db);
+	
+	// check on the number of records returned
+	if (nTuples($result) == 0) {
+		// if we bet no records back, it means no records match the	AVAILSLOTID that we get
+		$isComplete = false;
+		$errorMessage .= "the AVAILSLOTID $AVAILSLOTID did not match any records in th AVAILABILITY_T table.";
+	}
+}
+
+// if we got this far and $isComplete is true it means we should delete the AVAILABILITY_T
+if ($isComplete) {
+	
+	//we will set up the delete statement to remove the movie from the database
+	$deletequery = "DELETE FROM AVAILABILITY_T WHERE AVAILSLOTID=$AVAILSLOTID";
 	//run the delete statement
 	queryDB($deletequery, $db);
 	
@@ -58,6 +74,13 @@ if ($isComplete) {
 	$response['status'] = 'success';
 	header('Content-Type: application/json');
 	echo(json_encode($response));
+} else {
+	// there's been an error. We need to report it the angular controller.
+	
+	// one of the things we want to send back is the data that this php file recieved.
+	ob_start();
+	var_dump($data);
+	$postdump = ob_get_clean();
 	
 	// set up our response array
 	$response = array();
